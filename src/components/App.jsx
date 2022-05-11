732e4817d529713679ac4db554fc6633b29e3ee3
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-05-11 22:31:04 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './contactForm/contactForm';
+import ContactsList from './contactList/contactList';
+import { Filter } from './contactFilter/contactFilter';
+import Notiflix from 'notiflix';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ id, name, number }) => {
+    const { contacts } = this.state;
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    )
+      return Notiflix.Notify.failure(`${name} is already in phonebook`);
+
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, { id, name, number }] };
+    });
+  };
+
+  removeContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  getContacts = () => {
+    const { filter, contacts } = this.state;
+    const filterNormalize = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterNormalize)
+    );
+  };
+
+  filterContacts = element => {
+    this.setState({ filter: element.currentTarget.value });
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitData={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.filterContacts} />
+        <ContactsList
+          contacts={this.getContacts()}
+          handleRemove={this.removeContact}
+        />
+      </>
+    );
+  }
+}
